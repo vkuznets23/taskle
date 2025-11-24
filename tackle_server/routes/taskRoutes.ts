cd89c02d8eb69e646b93140bb640442f67cdd9c5
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-11-24 12:32:43 +0200

    deployment

--- tackle_server/routes/taskRoutes.ts
+++ tackle_server/routes/taskRoutes.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/prisma'
 import express from 'express'
 import { authenticateToken } from '../middlewares/auth.js'
 
--- tackle_server/routes/user.ts
+++ tackle_server/routes/user.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/prisma'
 import bcrypt from 'bcrypt'
 import express from 'express'
 import jwt, { JwtPayload } from 'jsonwebtoken'
