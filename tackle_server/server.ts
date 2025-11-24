384f53da89a7f2bfd218484a34941ed12b4b3819
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-11-24 11:40:56 +0200

    domen changed

--- tackle_server/server.ts
+++ tackle_server/server.ts
@@ -10,7 +10,7 @@ const app = express()
 // Middleware
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://taskly-eosin-ten.vercel.app'],
     credentials: true,
   })
 )
